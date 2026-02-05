bc8f89979d01c96745106a54ac7136f4e37da092
elecodes <186415469+elecodes@users.noreply.github.com>
2026-02-05 21:48:35 +0100

    fix: exclude E2E tests from vitest

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    exclude: ['node_modules', 'dist', 'tests/e2e/**'],
     setupFiles: './src/vitest.setup.js',
     coverage: {
       provider: 'v8',
